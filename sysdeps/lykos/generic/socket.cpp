e97ea90a37f00bad0e45a69dd72795c292a9aeb6
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2026-03-18 12:17:26 +0200

    socket: make syscalls

--- sysdeps/lykos/generic/socket.cpp
+++ sysdeps/lykos/generic/socket.cpp
@@ -14,75 +14,53 @@ namespace mlibc
 {
     int sys_accept(int fd, int *newfd, struct sockaddr *addr_ptr, socklen_t *addr_length, int flags)
     {
-		long ret;
-		long error = syscall(SYSCALL_ACCEPT, &ret, fd, (uint64_t)addr_ptr, (uint64_t)addr_length, flags);
-		*newfd = ret;
-		return error;
+		syscall_ret_t ret = syscall4(SYSCALL_ACCEPT, fd, (uint64_t)addr_ptr, (uint64_t)addr_length, flags);
+		*newfd = ret.value;
+		return ret.error;
 	}
 
 	int sys_bind(int fd, const struct sockaddr *addr_ptr, socklen_t addr_length)
 	{
-		long ret;
-		return syscall(SYSCALL_BIND, &ret, fd, (uint64_t)addr_ptr, addr_length);
+		syscall_ret_t ret = syscall2(SYSCALL_BIND, fd, (uint64_t)addr_ptr);
+		return ret.error;
 	}
 
 	int sys_connect(int fd, const struct sockaddr *addr_ptr, socklen_t addr_length)
 	{
-		long ret;
-		return syscall(SYSCALL_CONNECT, &ret, fd, (uint64_t)addr_ptr, addr_length);
+		syscall_ret_t ret =  syscall2(SYSCALL_CONNECT, fd, (uint64_t)addr_ptr);
+		return ret.error;
 	}
 
 	int sys_listen(int fd, int backlog)
 	{
-		long ret;
-		return syscall(SYSCALL_LISTEN, &ret, fd, backlog);
-	}
-
-	int sys_msg_recv(int fd, struct msghdr *hdr, int flags, ssize_t *length)
-	{
-		long ret;
-		long err = syscall(SYSCALL_RECVMSG, &ret, fd, (uint64_t)hdr, flags);
-		*length = ret;
-		return err;
-	}
-
-	int sys_msg_send(int fd, const struct msghdr *hdr, int flags, ssize_t *length)
-	{
-		long ret;
-		long err = syscall(SYSCALL_SENDMSG, &ret, fd, (uint64_t)hdr, flags);
-		*length = ret;
-		return err;
+		syscall_ret_t ret = syscall2(SYSCALL_LISTEN, fd, backlog);
+		return ret.error;
 	}
 
 	int sys_peername(int fd, struct sockaddr *addr_ptr, socklen_t max_addr_length, socklen_t *actual_length)
 	{
-		long ret;
-		int len = max_addr_length;
-		long error = syscall(SYSCALL_GETPEERNAME, &ret, fd, (uint64_t)addr_ptr, (uint64_t)&len);
-		*actual_length = len;
-		return error;
+		syscall_ret_t ret = syscall3(SYSCALL_GETPEERNAME, fd, (uint64_t)addr_ptr, (uint64_t)max_addr_length);
+		*actual_length = ret.value;
+		return ret.error;
 	}
 
 	int sys_shutdown(int sockfd, int how)
 	{
-		long ret;
-		return syscall(SYSCALL_SHUTDOWN, &ret, sockfd, how);
+	    syscall_ret_t ret = syscall2(SYSCALL_SHUTDOWN, sockfd, how);
+		return ret.error;
 	}
 
 	int sys_socket(int family, int type, int protocol, int *fd)
 	{
-		long ret;
-		long err = syscall(SYSCALL_SOCKET, &ret, family, type, protocol);
-		*fd = ret;
-		return err;
+		syscall_ret_t ret = syscall3(SYSCALL_SOCKET, family, type, protocol);
+		*fd = ret.value;
+		return ret.error;
 	}
 
 	int sys_sockname(int fd, struct sockaddr *addr_ptr, socklen_t max_addr_length, socklen_t *actual_length)
 	{
-		long ret;
-		int len = max_addr_length;
-		long error = syscall(SYSCALL_GETSOCKNAME, &ret, fd, (uint64_t)addr_ptr, (uint64_t)&len);
-		*actual_length = len;
-		return error;
+		syscall_ret_t ret = syscall3(SYSCALL_GETSOCKNAME, fd, (uint64_t)addr_ptr, (uint64_t)max_addr_length);
+		*actual_length = ret.value;
+		return ret.error;
 	}
 }
